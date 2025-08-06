4ee40b37065ede3e6eff2245da8e973d55241f8f
Алексей Бочкарев <105438413+alekseiBochkarev@users.noreply.github.com>
2025-08-06 09:20:58 +0700

    fix tests

--- tests/api/test_users_api.py
+++ tests/api/test_users_api.py
@@ -1,17 +1,16 @@
 import requests
+from http import HTTPStatus
 from models.user_models import User
 
-BASE_URL = "http://localhost:8000"
 
-
-def test_get_user_by_id_success(db_session):
+def test_get_user_by_id_success(db_session, app_url):
     # 1. Получаем юзера из базы
     user = db_session.query(User).first()
     assert user is not None, "В базе нет ни одного пользователя"
 
     # 2. Делаем GET-запрос по ID
-    response = requests.get(f"{BASE_URL}/api/users/{user.id}")
-    assert response.status_code == 200
+    response = requests.get(f"{app_url}/api/users/{user.id}")
+    assert response.status_code == HTTPStatus.OK
 
     data = response.json()["data"]
 
@@ -23,12 +22,12 @@ def test_get_user_by_id_success(db_session):
     assert data["avatar"] == user.avatar
 
 
-def test_get_user_not_found(db_session):
+def test_get_user_not_found(db_session, app_url):
     max_id = db_session.query(User.id).order_by(User.id.desc()).first()
     # 2. Выбираем ID, которого точно нет
     non_existing_id = (max_id[0] if max_id else 0) + 1000
 
     # 3. Делаем запрос
-    response = requests.get(f"{BASE_URL}/api/users/{non_existing_id}")
+    response = requests.get(f"{app_url}/api/users/{non_existing_id}")
     assert response.status_code == 404
     assert response.json()["detail"] == "User not found"
